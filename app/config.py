2a34c6123d1e62ee54eb579ef3e79f46adcdbf41
Nguyen <nvnguyen0089@gmail.com>
2024-04-06 17:02:41 +0700

    Setup logging

--- app/config.py
+++ app/config.py
@@ -1,9 +1,16 @@
 import os
 
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
-class Settings:
-    PROJECT_NAME = os.getenv("PROJECT_NAME", "FASTAPI-PROJECT-STRUCTURE")
-    API_PREFIX = "/api/v1"
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(env_prefix="")
+
+    project_name: str = os.getenv("PROJECT_NAME", "FASTAPI-PROJECT-STRUCTURE")
+    api_prefix: str = "/api/v1"
+    logging_config_file: str = os.path.join(BASE_DIR, "logging.yaml")
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+import logging
+import sys
 from fastapi import FastAPI
+import yaml
 
 from app.config import settings
 from app.containers import Container
@@ -8,23 +11,36 @@ from app.exception_handlers import (
 )
 
 
+def setup_logger(config_file: str, default_level=logging.INFO) -> None:
+    with open(config_file, mode="r", encoding="utf-8") as f:
+        try:
+            config = yaml.safe_load(f.read())
+            logging.config.dictConfig(config)
+        except Exception as e:
+            print("Failed to load configuration file. Using default configs", e)
+            logging.basicConfig(level=default_level, stream=sys.stdout)
+
+
 def create_app() -> FastAPI:
     _ = Container()
     application = FastAPI(
-        title=settings.PROJECT_NAME,
+        title=settings.project_name,
         docs_url="/docs",
         redoc_url="/re-docs",
-        openapi_url=f"{settings.API_PREFIX}/openapi.json",
+        openapi_url=f"{settings.api_prefix}/openapi.json",
         description="""
         The fastapi project template
         """,
     )
 
+    # Set logger
+    setup_logger(config_file=settings.logging_config_file)
+
     # Register exception handlers
     register_global_error_handlers(app=application)
 
     # application.add_middleware(DBSessionMiddleware, db_url=settings.DATABASE_URL)
-    application.include_router(user_router, prefix=settings.API_PREFIX)
+    application.include_router(user_router, prefix=settings.api_prefix)
     # application.add_exception_handler(CustomException, http_exception_handler)
 
     return application
